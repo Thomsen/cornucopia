4490ae65fb7be32b79b2d81da6dc00e0b6a8ddaa
Thomsen <changsen4584@163.com>
2011-12-30 23:08:59 +0800

    activity testing

---
+++ basic/Activity/src/com/cornucopia/activity/ActivityTest.java
@@ -0,0 +1,17 @@
+package com.cornucopia.activity;
+
+import com.cornucopia.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityTest extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_test);
+	}
+
+}
