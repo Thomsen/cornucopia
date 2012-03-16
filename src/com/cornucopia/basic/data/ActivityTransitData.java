a898968980df3d46f98d04bc02a19ff22241097c
Thomsen <changsen4584@163.com>
2012-03-16 15:31:10 +0800

    developer resources demo

---
+++ src/com/cornucopia/basic/data/ActivityTransitData.java
@@ -0,0 +1,9 @@
+package com.cornucopia.basic.data;
+
+import android.app.Activity;
+
+public class ActivityTransitData extends Activity {
+	
+	// 注意Intent的setFlags对putExtra的影响
+
+}
---
+++ src/com/cornucopia/multimedia/photointent/FroyoAlbumDirFactory.java
@@ -0,0 +1,20 @@
+package com.cornucopia.multimedia.photointent;
+
+import java.io.File;
+
+
+import android.os.Environment;
+
+public final class FroyoAlbumDirFactory extends AlbumStorageDirFactory {
+
+	@Override
+	public File getAlbumStorageDir(String albumName) {
+		// TODO Auto-generated method stub
+		return new File(
+		  Environment.getExternalStoragePublicDirectory(
+		    Environment.DIRECTORY_PICTURES // level 8
+		  ), 
+		  albumName
+		);
+	}
+}
