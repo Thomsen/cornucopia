ff61ffd8dd2f584b5f44d25cf11b2c9c80abd483
unknown <Thomsen@.(none)>
2012-05-06 17:32:33 +0800

    communication recognition

--- src/com/cornucopia/apache/http/Communication.java
+++ src/com/cornucopia/apache/http/Communication.java
@@ -8,61 +8,90 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ScrollView;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Communication extends Activity {
+import com.cornucopia.R;
+
+public class Communication extends Activity implements OnClickListener {
 
 	TextView mTextView;
+	
+	Button mButtonUrlGet;
+	Button mButtonUrlPost;
+	Button mButtonHttpGet;
+	Button mButtonHttpPost;
 
-	// webview测试
-	private String url01 = "192.168.56.1"; // no
-	private String url02 = "http://192.168.56.1:8089"; // yes
-	private String url03 = "http://192.168.56.1:3001"; // yes
-	private String url04 = "http://192.168.56.1:3003"; // yes
-
-	// android的内置ip
-	private String url05 = "http://10.0.2.2"; // yes
-	private String url06 = "http://10.0.2.2:8089"; // yes
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// testWebViewToServer();
-
-		ScrollView sv = new ScrollView(this);
-
-		mTextView = new TextView(this);
-
-		// mTextView.setText(getResultWithHttpConnection(url05));
-		mTextView.setText(getResultWithHttpClient(url06));
-
-		sv.addView(mTextView);
-
-		setContentView(sv);
+		setContentView(R.layout.apache_communication);
+		
+		setupViews();
+	}
+	
+	@Override
+	public void onClick(View v) {
+		String url = "http://www.baidu.com";
+		
+		switch (v.getId()) {
+		case R.id.conn_url_get: {
+			mTextView.setText(getResultWithUrlGet(url));
+			break;
+		}
+		case R.id.conn_url_post: {
+			mTextView.setText(getResultWithUrlPost(url));
+			break;
+		}
+		case R.id.conn_http_get: {
+//			url = "http://www.baidu.com";
+			mTextView.setText(getResultWithHttpGet(url));
+			break;
+		}
+		case R.id.conn_http_post: {
+			mTextView.setText(getResultWithHttpPost(url));
+			break;
+		}
+		}
+		
+	}
+	
+	private void setupViews() {
+		mButtonUrlGet = (Button) findViewById(R.id.conn_url_get);
+		mButtonUrlPost = (Button) findViewById(R.id.conn_url_post);
+		mButtonHttpGet = (Button) findViewById(R.id.conn_http_get);
+		mButtonHttpPost = (Button) findViewById(R.id.conn_http_post);
+		
+		mButtonUrlGet.setOnClickListener(this);
+		mButtonUrlPost.setOnClickListener(this);
+		mButtonHttpGet.setOnClickListener(this);
+		mButtonHttpPost.setOnClickListener(this);
+		
+		mTextView = (TextView) findViewById(R.id.conn_content);
 	}
 
-	private String getResultWithHttpConnection(String strUrl) {
-		String result = "communication http connection";
+	private String getResultWithUrlGet(String strUrl) {
+		String result = "communication url get";
 
 		try {
 			URL url = new URL(strUrl);
@@ -99,17 +128,17 @@ public class Communication extends Activity {
 		return result;
 	}
 
-	private String getResultWithHttpClient(String strUrl) {
+	private String getResultWithHttpGet(String strUrl) {
 
-		String result = "communication http client";
+		String result = "communication http get";
 
 		HttpClient httpClient = new DefaultHttpClient();
 
 		// get方式
-		// HttpGet httpGet = new HttpGet(strUrl);
+		 HttpGet httpGet = new HttpGet(strUrl);
 
 		// post方式
-		HttpPost httpPost = new HttpPost(strUrl);
+//		HttpPost httpPost = new HttpPost(strUrl);
 
 		// 设置连接和读取参数
 		HttpParams params = httpClient.getParams();
@@ -117,8 +146,8 @@ public class Communication extends Activity {
 		HttpConnectionParams.setSoTimeout(params, 1 * 1000);
 
 		try {
-			// HttpResponse httpResponse = httpClient.execute(httpGet);
-			HttpResponse httpResponse = httpClient.execute(httpPost);
+			 HttpResponse httpResponse = httpClient.execute(httpGet);
+//			HttpResponse httpResponse = httpClient.execute(httpPost);
 
 			HttpEntity httpEntity = httpResponse.getEntity();
 
@@ -129,17 +158,23 @@ public class Communication extends Activity {
 				result = convertStreamToString(inStream);
 
 			}
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return result;
 	}
+	
+	private CharSequence getResultWithHttpPost(String url) {
+		
+		String result = "communication http post";
+
+		
+		return result;
+	}
 
 	private String convertStreamToString(InputStream inStream) {
-		String str = "";
+		String result = "result";
 
 		try {
 			InputStreamReader inReader = new InputStreamReader(inStream,
@@ -151,19 +186,25 @@ public class Communication extends Activity {
 				strBuffer.append(s);
 			}
 
-			str = strBuffer.toString();
+			result = strBuffer.toString();
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		return str;
+		return result;
+	}
+	
+	private CharSequence getResultWithUrlPost(String url) {
+		String result = "communication url post";
+
+		return result;
 	}
 
-	private void testWebViewToServer() {
+	public void testWebViewToServer() {
 		WebView webView = new WebView(this);
-		webView.loadUrl(url04);
+		webView.loadUrl("http://www.baidu.com");
 
 		// 一种方式
 		final Activity activity = this;
@@ -184,5 +225,7 @@ public class Communication extends Activity {
 
 		setContentView(webView);
 	}
+
+
 
 }
---
+++ src/com/cornucopia/multimedia/SpeakAndTextActivity.java
@@ -0,0 +1,145 @@
+package com.cornucopia.multimedia;
+
+import java.util.List;
+import java.util.Locale;
+
+import com.cornucopia.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SpeakAndTextActivity extends Activity implements OnClickListener {
+
+	EditText mEditText;
+	Button mButtonSTT;
+	Button mButtonTTS;
+
+	TextToSpeech mTextToSpeech;
+
+	private static final int STT_RECOGNITION = 0x00000000;
+	private static final int TTS_RECOGNITION = 0x00000010;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.multimedia_stts);
+
+		setupViews();
+
+		initTTSRecognition();
+
+	}
+
+	private void setupViews() {
+		mEditText = (EditText) findViewById(R.id.multimedia_content);
+
+		mButtonSTT = (Button) findViewById(R.id.multimedia_stt);
+		mButtonTTS = (Button) findViewById(R.id.multimedia_tts);
+
+		mButtonSTT.setOnClickListener(this);
+		mButtonTTS.setOnClickListener(this);
+
+		PackageManager pm = getPackageManager();
+		List activityList = pm.queryIntentActivities(new Intent(
+				RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+
+		if (activityList.size() != 0) {
+			mButtonSTT.setEnabled(true);
+		} else {
+			mButtonSTT.setEnabled(false);
+		}
+
+	}
+
+	@Override
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		case R.id.multimedia_stt: {
+			startSTTRecognition();
+			break;
+		}
+		case R.id.multimedia_tts: {
+			startTTSRecognition();
+			break;
+		}
+		default:
+			break;
+		}
+
+	}
+
+	private void startTTSRecognition() {
+
+		mTextToSpeech.speak(mEditText.getText().toString(),
+				TextToSpeech.QUEUE_ADD, null);
+
+	}
+
+	private void startSTTRecognition() {
+		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+		startActivityForResult(intent, STT_RECOGNITION);
+
+		mTextToSpeech.setLanguage(Locale.US);
+
+	}
+
+	private void initTTSRecognition() {
+		Intent intent = new Intent(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+		startActivityForResult(intent, TTS_RECOGNITION);
+
+		
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		if (requestCode == STT_RECOGNITION) {
+
+			switch (resultCode) {
+			case RESULT_OK: {
+				// TODO use adapter to select
+				mEditText.setText(data.getStringArrayListExtra(
+						RecognizerIntent.EXTRA_RESULTS).toString());
+				break;
+			}
+			}
+
+		} else if (requestCode == TTS_RECOGNITION) {
+			switch (resultCode) {
+			case TextToSpeech.Engine.CHECK_VOICE_DATA_PASS: {
+				mTextToSpeech = new TextToSpeech(this, new OnInitListener() {
+
+					@Override
+					public void onInit(int status) {
+						if (status == TextToSpeech.SUCCESS) {
+							// replace Locale.US
+							int result = mTextToSpeech.setLanguage(Locale.US);
+							if (result == TextToSpeech.LANG_MISSING_DATA
+									|| result == TextToSpeech.LANG_NOT_SUPPORTED) {
+								mButtonTTS.setEnabled(false);
+								
+								// TODO setting language
+								mTextToSpeech.setLanguage(Locale.US);
+							} else {
+								mButtonTTS.setEnabled(true);
+							}
+						}
+					}
+
+				});
+				break;
+			}
+			}
+		}
+	}
+}
