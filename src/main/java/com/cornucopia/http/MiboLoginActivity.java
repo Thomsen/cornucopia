7a307222cb4ef8f74737fba2b40bc65f03f0790a
Thomsen <changsen4584@163.com>
2014-12-31 11:22:09 +0800

    refactor http communication and add okhttp

--- src/main/java/com/cornucopia/http/MiboLoginActivity.java
+++ src/main/java/com/cornucopia/http/MiboLoginActivity.java
@@ -20,8 +20,11 @@ import com.android.volley.toolbox.HttpStack;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.cornucopia.R;
+import com.cornucopia.http.appache.HttpOpt;
+import com.cornucopia.http.okhttp.OkHttpOpt;
 import com.cornucopia.http.volley.GsonRequest;
 import com.cornucopia.http.volley.MiboUser;
+import com.cornucopia.http.volley.VolleyOpt;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 
@@ -91,8 +94,8 @@ public class MiboLoginActivity extends Activity implements OnClickListener {
 //                	login();
 //                }
 //            }.start();
-            
-            volleyLogin();
+//            volleyLogin();
+            okHttpLogin();
         }
     }
     
@@ -100,7 +103,8 @@ public class MiboLoginActivity extends Activity implements OnClickListener {
     	String showUrl = serverAddress + "/users/17";
     	HttpParams params = new BasicHttpParams();
 //    	params.setIntParameter("id", 17);
-    	httpGet(showUrl, params);
+    	HttpOpt hopt = new HttpOpt(this);
+    	hopt.httpGet(showUrl, params);
     }
     
     private void login() {
@@ -108,145 +112,40 @@ public class MiboLoginActivity extends Activity implements OnClickListener {
     	ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
     	params.add(new BasicNameValuePair("username", mEtUsername.getText().toString()));
     	params.add(new BasicNameValuePair("password", mEtPassword.getText().toString()));
-    	httpPost(loginUrl, params);
+    	HttpOpt hopt = new HttpOpt(this);
+    	hopt.httpPost(loginUrl, params);
     }
     
     private void volleyShow() {
         String showUrl = serverAddress + "/users/1";
         JSONObject jsonRequest = new JSONObject();
-        volleyGet(showUrl, jsonRequest);
+        VolleyOpt vopt = new VolleyOpt(this);
+        vopt.volleyGet(showUrl, jsonRequest);
     }
     
     private void volleyLogin() {
+        VolleyOpt vopt = new VolleyOpt(this);
         String loginUrl = serverAddress + "/users/login";
-        HashMap<String, String> params = new HashMap<String, String>();
-        params.put("username", mEtUsername.getText().toString());
-        params.put("password", mEtPassword.getText().toString());
-        JSONObject jsonRequest = new JSONObject(params);
-        TypeToken<MiboUser> typeOf = new TypeToken<MiboUser>(){};
-        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-        queue.add(new GsonRequest<>(Method.POST, loginUrl, typeOf, null, jsonRequest, new Response.Listener<MiboUser>() {
-
-            @Override
-            public void onResponse(MiboUser response) {
-                Log.i("thom", "username " + response.getUsername());
-                Toast.makeText(getApplicationContext(), "username " + response.getUsername(),
-                        Toast.LENGTH_SHORT).show();
-            }
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("thom", "error response " + error.getMessage());
-            }
-        }));
-        queue.start();
-        
+        vopt.volleyLogin(loginUrl, mEtUsername.getText().toString(),
+                mEtPassword.getText().toString());
     }
     
-    private void httpGet(String url, HttpParams params) {
-        HttpClient httpClient = new DefaultHttpClient(params);
-        HttpGet httpGet = new HttpGet(url);
-        
-        HttpParams getParams = httpClient.getParams();
-        httpGet.setParams(getParams);
-        
-        String response = "";
-        try {
-			HttpResponse httpResp = httpClient.execute(httpGet);
-			if (httpResp.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-				HttpEntity httpEntity = httpResp.getEntity();
-				InputStream inStream = httpEntity.getContent();
-				response = convertStreamToString(inStream);
-				Log.d("thom", "get request " + response);
-			} else {
-				Log.e("thom", "get request bad!");
-			}
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        
+    private void okHttpShow() {
+        new Thread() {
+            public void run() {
+                String showUrl = serverAddress + "/users/6";
+                OkHttpOpt oopt = new OkHttpOpt(MiboLoginActivity.this);
+                String resp = oopt.okHttpGet(showUrl);
+                Log.d("thom", "ok http resp: " + resp);
+            }
+        }.start();
     }
     
-    private void httpPost(String url, List<NameValuePair> params) {
-        HttpClient httpClient = new DefaultHttpClient();
-        
-        HttpPost httpPost = new HttpPost(url);
-        try {
-			httpPost.setEntity(new UrlEncodedFormEntity(params));
-		} catch (UnsupportedEncodingException e1) {
-			e1.printStackTrace();
-		}
-        
-        try {
-            HttpResponse httpResp = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResp.getEntity();
-            if (null != httpEntity) {
-                InputStream inStream = httpEntity.getContent();
-                final String response = convertStreamToString(inStream);
-                
-                Log.i("thom", "response: " + response);
-                runOnUiThread(new Runnable() {
-
-        			@Override
-        			public void run() {
-        				Toast.makeText(MiboLoginActivity.this, response, Toast.LENGTH_SHORT).show();
-        			}
-                	
-                });
-            }
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-    }
-    
-    private void volleyGet(String url, JSONObject jsonRequest) {
-        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-        queue.add(new JsonObjectRequest(Method.GET, url, jsonRequest,
-          new Response.Listener<JSONObject>() {
-
-            @Override
-            public void onResponse(JSONObject response) {
-                Log.i("thom", "volley login " + response);
-                Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_SHORT).show();
-            }
-            
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("thom", "message " + error.getMessage());
-            }
-        }));
-        queue.start();
-    }
-    
-    private String convertStreamToString(InputStream inStream) {
-        String result = "result";
-
-        try {
-            InputStreamReader inReader = new InputStreamReader(inStream,
-                    "UTF-8");
-            BufferedReader bufferReader = new BufferedReader(inReader);
-            StringBuffer strBuffer = new StringBuffer();
-            String s;
-            while ((s = bufferReader.readLine()) != null) {
-                strBuffer.append(s);
-            }
-
-            result = strBuffer.toString();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return result;
+    private void okHttpLogin() {
+        OkHttpOpt oopt = new OkHttpOpt(this);
+        String loginUrl = serverAddress + "/users/login";
+        oopt.okHttpLogin(loginUrl, mEtUsername.getText().toString(),
+                mEtPassword.getText().toString());
     }
     
 }
---
+++ src/main/java/com/cornucopia/utils/StringUtils.java
@@ -0,0 +1,35 @@
+package com.cornucopia.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+public class StringUtils {
+
+    private StringUtils() {};
+    
+    public static String convertStreamToString(InputStream inStream) {
+        String result = "result";
+
+        try {
+            InputStreamReader inReader = new InputStreamReader(inStream,
+                    "UTF-8");
+            BufferedReader bufferReader = new BufferedReader(inReader);
+            StringBuffer strBuffer = new StringBuffer();
+            String s;
+            while ((s = bufferReader.readLine()) != null) {
+                strBuffer.append(s);
+            }
+
+            result = strBuffer.toString();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
