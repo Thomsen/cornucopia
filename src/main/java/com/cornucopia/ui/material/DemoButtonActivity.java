49fd79fda76e5d316288f77999c1d58a93654b29
Thomsen <changsen4584@163.com>
2015-01-06 14:53:08 +0800

    add apptheme and demo material button edittext

---
+++ src/main/java/com/cornucopia/ui/material/DemoButtonActivity.java
@@ -0,0 +1,17 @@
+package com.cornucopia.ui.material;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cornucopia.R;
+
+public class DemoButtonActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_material_button);
+    }
+
+}
