cc41916a29e05c50d898b50db7afcc2c16ef33bb
Thomsen <changsen4584@163.com>
2015-03-03 17:17:28 +0800

    eventbus demo

---
+++ src/main/java/com/cornucopia/event/MessageEvent.java
@@ -0,0 +1,11 @@
+package com.cornucopia.event;
+
+public class MessageEvent {
+
+    public final String message;
+
+    public MessageEvent(String message) {
+        this.message = message;
+    }
+
+}
