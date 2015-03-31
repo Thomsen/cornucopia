28d81e631c0790d536a97c09519b38df4389bff1
Thomsen <changsen4584@163.com>
2015-03-31 13:55:37 +0800

    viewpager pagetransformer animation

--- src/main/java/com/cornucopia/ui/fragment/ArticleFragment.java
+++ src/main/java/com/cornucopia/ui/fragment/ArticleFragment.java
@@ -2,6 +2,7 @@ package com.cornucopia.ui.fragment;
 
 import com.cornucopia.R;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,15 +11,35 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class ArticleFragment extends Fragment {
-	
-	int mCurrentPosition = -1;
-	
+
+	int mCurrentPosition = 0;
+
 	public static final String POSITION_STATE = "position";
-	
+
+	public ArticleFragment() {
+    }
+
+	public ArticleFragment(int position) {
+	    mCurrentPosition = position;
+    }
+
 	private String[] articles = new String[] {
 			"article one",
-			"article two"
+			"article two",
+			"article three",
+			"article four",
+			"article five"
 	};
+
+	private int[] colors = new int[] {
+	        Color.RED,
+	        Color.BLUE,
+	        Color.WHITE,
+	        Color.YELLOW,
+	        Color.GRAY
+	};
+
+	private ViewGroup rootView;
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -26,33 +47,37 @@ public class ArticleFragment extends Fragment {
 		if (savedInstanceState != null) {
 			mCurrentPosition = savedInstanceState.getInt(POSITION_STATE);
 		}
-		
-		return inflater.inflate(R.layout.ui_article_view, container, false);
 
+		rootView =  (ViewGroup) inflater.inflate(R.layout.ui_article_view, container, false);
+		return rootView;
 	}
-	
+
 	public void onStart() {
 		super.onStart();
-		
+
 		Bundle bundle = getArguments();
-		
-		if (bundle != null) {
-			updateArticleView(bundle.getInt(POSITION_STATE));
-		} else if (mCurrentPosition != -1)  {
+
+//		if (bundle != null) {
+//			updateArticleView(bundle.getInt(POSITION_STATE));
+//		} else if (mCurrentPosition != -1)  {
 			updateArticleView(mCurrentPosition);
-		}
-		
+//		}
+
 	}
-	
+
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
-		
+
 		outState.putInt(POSITION_STATE, mCurrentPosition);
 	}
 
 	public void updateArticleView(int position) {
-		TextView article = (TextView) getActivity().findViewById(R.id.article);
+//		TextView article = (TextView) getActivity().findViewById(R.id.article); // error
+	    TextView article = (TextView) rootView.findViewById(R.id.article);
 		article.setText(articles[position]);
-		mCurrentPosition = position;
+		article.invalidate();
+//		mCurrentPosition = position;
+
+		rootView.setBackgroundColor(colors[position]);
 	}
 }
