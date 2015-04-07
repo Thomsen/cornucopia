9fce9b72a2a32569fc3251acb4a2ca1c809024f6
Thomsen <changsen4584@163.com>
2015-04-07 15:55:59 +0800

    picture compress gray rgb yuv

--- src/main/java/com/cornucopia/graphics/compress/PictureCompressActivity.java
+++ src/main/java/com/cornucopia/graphics/compress/PictureCompressActivity.java
@@ -35,8 +35,10 @@ public class PictureCompressActivity extends FragmentActivity {
     private static final int TAKE_PICTURE = 023;
 
     private ImageView mImageOriginal;
-    private ImageView mImageCompress;
-    private ImageView mImageAndroid;
+    private ImageView mImageGrayCompress;
+    private ImageView mImageRgbCompress;
+    private ImageView mImageYuvCompress;
+    private ImageView mImageAndroidCompress;
 
     private ViewFlipper mFlipper;
 
@@ -52,8 +54,10 @@ public class PictureCompressActivity extends FragmentActivity {
         setContentView(R.layout.activity_picture_compress);
 
         mImageOriginal = (ImageView) findViewById(R.id.iv_image_original);
-        mImageCompress = (ImageView) findViewById(R.id.iv_image_compress);
-        mImageAndroid = (ImageView) findViewById(R.id.iv_image_android);
+        mImageGrayCompress = (ImageView) findViewById(R.id.iv_image_gray_compress);
+        mImageRgbCompress = (ImageView) findViewById(R.id.iv_image_rgb_compress);
+        mImageYuvCompress = (ImageView) findViewById(R.id.iv_image_yuv_compress);
+        mImageAndroidCompress = (ImageView) findViewById(R.id.iv_image_android_compress);
 
         startFlipper(this, true);
     }
@@ -141,10 +145,24 @@ public class PictureCompressActivity extends FragmentActivity {
 //                mImageCompress.setImageBitmap(compressBitmap(scaleBitmap()));  // oom
 //                mImageAndroid.setImageBitmap(androidCompressBitmap(scaleBitmap(), 80));
 
-                Bitmap bitmap = scaleBitmap(photoFile.getAbsolutePath());
-                mImageOriginal.setImageBitmap(bitmap);
-                mImageCompress.setImageBitmap(compressBitmap(bitmap));
-                mImageAndroid.setImageBitmap(androidCompressBitmap(bitmap, 80));
+//                Bitmap bitmap = scaleBitmap(photoFile.getAbsolutePath());
+//                int height = bitmap.getHeight();
+//                int width = bitmap.getWidth();
+
+                RenderPicture renderPic = new RenderPicture(photoFile.getAbsolutePath(), this);
+                PictureResults pr = renderPic.getPictureResults();
+                Bitmap bitmap = pr.getBitmap();
+                int height = pr.getHeight();
+                int width = pr.getWidth();
+
+                int[] pixs = new int[height * width];
+                bitmap.getPixels(pixs, 0, width, 0, 0, width, height);
+
+//                mImageOriginal.setImageBitmap(bitmap);
+                mImageGrayCompress.setImageBitmap(grayCompressBitmap(pixs, height, width, 0));
+                mImageRgbCompress.setImageBitmap(rgbCompressBitmap(pixs, height, width, 0));
+                mImageYuvCompress.setImageBitmap(yuvCompressBitmap(pixs, height, width, 0));
+                mImageAndroidCompress.setImageBitmap(androidCompressBitmap(bitmap, 80));
                 bitmap.recycle();
 
             }
@@ -191,8 +209,75 @@ public class PictureCompressActivity extends FragmentActivity {
         sendBroadcast(mediaIntent);
     }
 
-    private Bitmap compressBitmap(Bitmap bitmap) {
+    private Bitmap grayCompressBitmap(int[] bmpArray, int h, int w, int matrixChoice) {
         Bitmap compressBitmap = null;
+
+        DCT dct = new DCT();
+        Quantization quantRun = new Quantization(this, matrixChoice);
+
+        int[] tmp = quantRun.quantizer(dct.forwardDCT(bmpArray, h, w));
+        compressBitmap = dct.reverseDCT(tmp, h, w);
+        bitmapToFile(compressBitmap, "gray");
+
+        return compressBitmap;
+    }
+
+    private Bitmap rgbCompressBitmap(int[] bmpArray, int h, int w, int matrixChoice) {
+        Bitmap compressBitmap = null;
+
+        DCT dct = new DCT();
+        Quantization quantRun = new Quantization(this, matrixChoice);
+
+        double[][][] Y = dct.forwardRGBDCT(bmpArray, h, w);
+
+        int[] tmpR = quantRun.quantizer(Y[0]);
+        int[] tmpG = quantRun.quantizer(Y[1]);
+        int[] tmpB = quantRun.quantizer(Y[2]);
+
+        int[] R = dct.reverseDCT3D(tmpR, h, w);
+        int[] G = dct.reverseDCT3D(tmpG, h, w);
+        int[] B = dct.reverseDCT3D(tmpB, h, w);
+
+        int[] res = new int[h * w];
+
+        for (int i=0; i<tmpR.length; i++) {
+            res[i] = 0xff000000 | (R[i] << 16) | (G[i] << 8) | B[i];
+        }
+
+        compressBitmap = Bitmap.createBitmap(res, w, h, Bitmap.Config.ARGB_8888);
+        bitmapToFile(compressBitmap, "rgb");
+
+        return compressBitmap;
+    }
+
+    private Bitmap yuvCompressBitmap(int[] bmpArray, int h, int w, int matrixChoice) {
+        Bitmap compressBitmap = null;
+
+        DCT dct = new DCT();
+        Quantization quantRun = new Quantization(this, matrixChoice);
+
+        double[][][] YUV = dct.forwardYUVDCT(bmpArray, h, w);
+
+        int[] tmpY = quantRun.quantizer(YUV[0]);
+        int[] tmpU = quantRun.UVquantizer(YUV[1]);
+        int[] tmpV = quantRun.UVquantizer(YUV[2]);
+
+        int[] Y = dct.reverseDCT3D(tmpY, h, w);
+        int[] U = dct.reverseDCT3D(tmpU, h, w);
+        int[] V = dct.reverseDCT3D(tmpV, h, w);
+
+        int[] res = new int[h * w];
+
+        for (int i=0; i<tmpY.length; i++) {
+            int R = V[i] + Y[i];
+            int B = U[i] + Y[i];
+            int G = (int) ((Y[i] - .299 * R - .114 *B) / (.587));
+
+            res[i] = 0xff000000 | (R << 16) | (G << 8) | B;
+        }
+
+        compressBitmap = Bitmap.createBitmap(res, w, h, Bitmap.Config.ARGB_8888);
+        bitmapToFile(compressBitmap, "yuv");
 
         return compressBitmap;
     }
@@ -200,22 +285,38 @@ public class PictureCompressActivity extends FragmentActivity {
     private Bitmap androidCompressBitmap(Bitmap bitmap, int quality) {
         Bitmap compressBitmap = null;
 
-        BufferedOutputStream bos = null;
         ByteArrayOutputStream out = null;
-        FileOutputStream fos = null;
+
+        out = new ByteArrayOutputStream();
+        bitmap.compress(CompressFormat.JPEG, quality, out);
+
+        byte[] res = out.toByteArray();
+
+        byteArrayToFile(res, "android");
+        compressBitmap = BitmapFactory.decodeByteArray(res, 0, res.length);
+
+        return compressBitmap;
+
+    }
+
+    private void bitmapToFile(Bitmap bmp, String fileSuffix) {
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        bmp.compress(CompressFormat.JPEG, 100, out);
+
+        byte[] res = out.toByteArray();
+
+        byteArrayToFile(res, fileSuffix);
+    }
+
+    private void byteArrayToFile(byte[] res, String fileSuffix) {
+
+        BufferedOutputStream bos = null;
         try {
-            bos = new BufferedOutputStream(new FileOutputStream(touchFile("androidcompress")));
-
-            out = new ByteArrayOutputStream();
-            bitmap.compress(CompressFormat.JPEG, quality, out);
-
-            byte[] res = out.toByteArray();
-
-            compressBitmap = BitmapFactory.decodeByteArray(res, 0, res.length);
+            bos = new BufferedOutputStream(new FileOutputStream(touchFile(fileSuffix)));
             bos.write(res);
             bos.flush();
             bos.close();
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -228,16 +329,6 @@ public class PictureCompressActivity extends FragmentActivity {
                     e.printStackTrace();
                 }
             }
-            if (null != fos) {
-                try {
-                    bos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
-
-        return compressBitmap;
-
     }
  }
