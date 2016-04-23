ec16a43c28eb56b4ce797640ad8de83fcc3c4419
Thomsen <changsen4584@163.com>
2016-04-23 14:13:39 +0800

    intent system camera, camera take picture, camera2 capture picture

---
+++ copia-app/src/main/java/com/cornucopia/multimedia/camera2/ImageSaver.java
@@ -0,0 +1,44 @@
+package com.cornucopia.multimedia.camera2;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import android.media.Image;
+
+public class ImageSaver implements Runnable {
+
+    private final Image mImage;
+
+    private final File mFile;
+
+    public ImageSaver(Image image, File file) {
+        mImage = image;
+        mFile = file;
+    }
+
+    @Override
+    public void run() {
+        ByteBuffer buffer = mImage.getPlanes()[0].getBuffer();
+        byte[] bytes = new byte[buffer.remaining()];
+        buffer.get(bytes);
+        FileOutputStream output = null;
+        try {
+            output = new FileOutputStream(mFile);
+            output.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            mImage.close();
+            if (null != output) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+}
