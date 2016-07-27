00c0cf75da5c3c76a95f5057ff34e907da930c17
Thomsen <changsen4584@163.com>
2016-07-27 22:29:37 +0800

    android mpandroidchart and line chart

---
+++ copia-app/src/main/java/com/cornucopia/graphics/chart/LineChartActivity.java
@@ -0,0 +1,204 @@
+package com.cornucopia.graphics.chart;
+
+import java.util.ArrayList;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+
+import com.cornucopia.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.LimitLine.LimitLabelPosition;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.ChartTouchListener.ChartGesture;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.Utils;
+
+public class LineChartActivity extends FragmentActivity implements OnChartGestureListener, OnChartValueSelectedListener  {
+	
+	private LineChart mChart;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		setContentView(R.layout.activity_line_chart);
+		
+		mChart = (LineChart) findViewById(R.id.chart_line);
+	    mChart.setOnChartGestureListener(this);
+	    mChart.setOnChartValueSelectedListener(this);
+	    mChart.setDrawGridBackground(false);
+	        
+
+        // no description text
+        mChart.setDescription("");
+        mChart.setNoDataTextDescription("You need to provide data for the chart.");
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+        
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.enableGridDashedLine(10f, 10f, 0f);
+        
+        LimitLine ll1 = new LimitLine(150f, "Upper Limit");
+        ll1.setLineWidth(4f);
+        ll1.enableDashedLine(10f, 10f, 0f);
+        ll1.setLabelPosition(LimitLabelPosition.RIGHT_TOP);
+        ll1.setTextSize(10f);
+//        ll1.setTypeface(tf); // Typeface
+        
+        LimitLine ll2 = new LimitLine(-30f, "Lower Limit");
+        ll2.setLineWidth(4f);
+        ll2.enableDashedLine(10f, 10f, 0f);
+        ll2.setLabelPosition(LimitLabelPosition.RIGHT_BOTTOM);
+        ll2.setTextSize(10f);
+//        ll2.setTypeface(tf);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
+        leftAxis.addLimitLine(ll1);
+        leftAxis.addLimitLine(ll2);
+        leftAxis.setAxisMaxValue(200f);
+        leftAxis.setAxisMinValue(-50f);
+        //leftAxis.setYOffset(20f);
+        leftAxis.enableGridDashedLine(10f, 10f, 0f);
+        leftAxis.setDrawZeroLine(false);
+
+        // limit lines are drawn behind data (and not on top)
+        leftAxis.setDrawLimitLinesBehindData(true);
+
+        mChart.getAxisRight().setEnabled(false);
+        
+        setData(45, 100);
+	}
+	
+	private void setData(int count, float range) {
+
+        ArrayList<Entry> values = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+
+            float val = (float) (Math.random() * range) + 3;
+            values.add(new Entry(i, val));
+        }
+
+        LineDataSet set1;
+
+        if (mChart.getData() != null &&
+                mChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet)mChart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            mChart.getData().notifyDataChanged();
+            mChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(values, "DataSet 1");
+
+            // set the line to be drawn like this "- - - - - -"
+            set1.enableDashedLine(10f, 5f, 0f);
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+            set1.setColor(Color.BLACK);
+            set1.setCircleColor(Color.BLACK);
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(false);
+            set1.setValueTextSize(9f);
+            set1.setDrawFilled(true);
+
+            if (Utils.getSDKInt() >= 18) {
+                // fill drawable only supported on api level 18 and above
+                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
+                set1.setFillDrawable(drawable);
+            }
+            else {
+                set1.setFillColor(Color.BLACK);
+            }
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+            dataSets.add(set1); // add the datasets
+
+            // create a data object with the datasets
+            LineData data = new LineData(dataSets);
+
+            // set data
+            mChart.setData(data);
+        }
+    }
+
+	@Override
+	public void onValueSelected(Entry e, Highlight h) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onNothingSelected() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onChartGestureStart(MotionEvent me,
+			ChartGesture lastPerformedGesture) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onChartGestureEnd(MotionEvent me,
+			ChartGesture lastPerformedGesture) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onChartLongPressed(MotionEvent me) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onChartDoubleTapped(MotionEvent me) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onChartSingleTapped(MotionEvent me) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX,
+			float velocityY) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onChartTranslate(MotionEvent me, float dX, float dY) {
+		// TODO Auto-generated method stub
+		
+	}
+}
