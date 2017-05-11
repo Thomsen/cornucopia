b081cad3c57eee067f9f8df0fcc117c82c673d00
Thomsen <changsen4584@163.com>
2017-05-11 16:26:02 +0800

    activity and fragment lifecycle

--- copia-app/src/main/java/com/cornucopia/lifecycle/ActivityLifecycle.java
+++ copia-app/src/main/java/com/cornucopia/lifecycle/ActivityLifecycle.java
@@ -1,46 +1,55 @@
 package com.cornucopia.lifecycle;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.cornucopia.R;
 
 public class ActivityLifecycle extends Activity {
 	
-	public final static String TAG_ACTIVITY_LIFECYCLE = "tag_activity_lifecycle";
+	public final static String TAG_ACTIVITY_LIFECYCLE = "lifecycle";
 
 	@Override
 	public void onContentChanged() {
 		super.onContentChanged();
 		
-		Log.i(TAG_ACTIVITY_LIFECYCLE, "onContentChanged run...");
+		Log.i(TAG_ACTIVITY_LIFECYCLE, "activity content changed");
 	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		LinearLayout linearLayout01 = new LinearLayout(this);
-		
-		TextView textView = new TextView(this);
+//		LinearLayout linearLayout01 = new LinearLayout(this);
+//		TextView textView = new TextView(this);
+//		textView.setText("Activity Lifecycle");
+//		linearLayout01.addView(textView);
+//		setContentView(linearLayout01);
+
+		setContentView(R.layout.activity_fragment_lifecycle);
+
+		TextView textView = (TextView) findViewById(R.id.tv_lifecycle);
 		textView.setText("Activity Lifecycle");
-		
-		linearLayout01.addView(textView);
-		
-		setContentView(linearLayout01);
+
+		FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+		fragmentTransaction.add(R.id.frame_lifecycle, new FragmentLifecycle());
+		fragmentTransaction.commit();
+
 		
 		// The activity is being created.
 		
-		Log.i(TAG_ACTIVITY_LIFECYCLE, "onCreate run...");
+		Log.i(TAG_ACTIVITY_LIFECYCLE, "activity create");
 	}
 	
 	@Override
 	protected void onRestart() {
 		super.onRestart();
 		
-		Log.i(TAG_ACTIVITY_LIFECYCLE, "onRestart run...");
+		Log.i(TAG_ACTIVITY_LIFECYCLE, "activity restart");
 	}
 	
 	@Override
@@ -49,21 +58,21 @@ public class ActivityLifecycle extends Activity {
 		
 		// The activity is about to become visible.
 		
-		Log.i(TAG_ACTIVITY_LIFECYCLE, "onStart run...");
+		Log.i(TAG_ACTIVITY_LIFECYCLE, "activity start");
 	}
 	
 	@Override
 	protected void onRestoreInstanceState(Bundle savedInstanceState) {
 		super.onRestoreInstanceState(savedInstanceState);
 		
-		Log.i(TAG_ACTIVITY_LIFECYCLE, "onRestoreInstanceState run...");
+		Log.i(TAG_ACTIVITY_LIFECYCLE, "activity restore instance state");
 	}
 
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
 		
-		Log.i(TAG_ACTIVITY_LIFECYCLE, "onPostCreate run...");
+		Log.i(TAG_ACTIVITY_LIFECYCLE, "activity post create");
 	}
 
 	@Override
@@ -72,7 +81,7 @@ public class ActivityLifecycle extends Activity {
 		
 		// The activity has become visible (it is now "resumed").
 
-		Log.i(TAG_ACTIVITY_LIFECYCLE, "onResume run...");
+		Log.i(TAG_ACTIVITY_LIFECYCLE, "activity resume");
 	}
 	
 
@@ -80,7 +89,7 @@ public class ActivityLifecycle extends Activity {
 	protected void onPostResume() {
 		super.onPostResume();
 		
-		Log.i(TAG_ACTIVITY_LIFECYCLE, "onPostResume run...");
+		Log.i(TAG_ACTIVITY_LIFECYCLE, "activity post resume");
 		Log.i(TAG_ACTIVITY_LIFECYCLE, "--------------------------------");
 	}
 
@@ -90,14 +99,14 @@ public class ActivityLifecycle extends Activity {
 		
 		// Another activity is taking focus (this activity is about to be "paused").
 		
-		Log.i(TAG_ACTIVITY_LIFECYCLE, "onPause run...");
+		Log.i(TAG_ACTIVITY_LIFECYCLE, "activity pause");
 	}
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 		
-		Log.i(TAG_ACTIVITY_LIFECYCLE, "onSaveInstanceState run...");
+		Log.i(TAG_ACTIVITY_LIFECYCLE, "activity save instance state");
 	}
 	
 	@Override
@@ -106,7 +115,7 @@ public class ActivityLifecycle extends Activity {
 		
 		// The activity is no longer visible (it is now "stopped")
 
-		Log.i(TAG_ACTIVITY_LIFECYCLE, "onStop run...");
+		Log.i(TAG_ACTIVITY_LIFECYCLE, "activity stop");
 	}
 	
 	@Override
@@ -115,7 +124,7 @@ public class ActivityLifecycle extends Activity {
 		
 		// The activity is about to be destroyed.
 
-		Log.i(TAG_ACTIVITY_LIFECYCLE, "onDestory run...");
+		Log.i(TAG_ACTIVITY_LIFECYCLE, "activity destroy");
 		Log.i(TAG_ACTIVITY_LIFECYCLE, "--------------------------------");
 	}
 
@@ -123,12 +132,11 @@ public class ActivityLifecycle extends Activity {
 	/*
 	 *  横屏切换
 	 */
-
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
 		
-		Log.i(TAG_ACTIVITY_LIFECYCLE, "onConfigurationChanged run...");
+		Log.i(TAG_ACTIVITY_LIFECYCLE, "activity configuration changed");
 	
 		if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
 			Log.d(TAG_ACTIVITY_LIFECYCLE, "orientation landcape");
---
+++ copia-app/src/main/java/com/cornucopia/lifecycle/FragmentLifecycle.java
@@ -0,0 +1,101 @@
+package com.cornucopia.lifecycle;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cornucopia.R;
+
+/**
+ * Created by thom on 9/5/2017.
+ */
+
+public class FragmentLifecycle extends Fragment {
+
+    private static final String TAG = "lifecycle";
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        Log.i(TAG, "fragment attach");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.i(TAG, "fragment create");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        Log.i(TAG, "fragmen create view");
+
+//        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.activity_fragment_lifecycle, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Log.i(TAG, "fragment activity created");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "fragment start");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "fragment resume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        Log.i(TAG, "fragment pause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        Log.i(TAG, "fragment stop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        Log.i(TAG, "fragment destroy view");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Log.i(TAG, "fragment destroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        Log.i(TAG, "fragment detach");
+    }
+}
