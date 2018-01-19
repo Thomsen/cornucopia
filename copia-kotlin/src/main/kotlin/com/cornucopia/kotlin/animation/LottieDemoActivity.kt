fd2baef18a6f79fae7d3788e975433a09474f96a
Thomsen <changsen4584@163.com>
2018-01-19 18:29:55 +0800

    lottie xml demo

---
+++ copia-kotlin/src/main/kotlin/com/cornucopia/kotlin/animation/LottieDemoActivity.kt
@@ -0,0 +1,21 @@
+package com.cornucopia.kotlin.animation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.cornucopia.kotlin.R
+
+import kotlinx.android.synthetic.main.activity_lottie_demo.*;
+
+/**
+ * Created by thom on 19/1/2018.
+ */
+class LottieDemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_lottie_demo)
+
+//        anim_lottie
+    }
+}
