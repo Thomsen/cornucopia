e31aed65d52352b46bb80e1b1f1baab0430dcd16
Thomsen <changsen4584@163.com>
2018-06-12 17:16:28 +0800

    weather data list adapter

--- copia-kotlin/src/main/kotlin/com/cornucopia/kotlin/weather/viewmodel/view/ForecastAdapter.kt
+++ copia-kotlin/src/main/kotlin/com/cornucopia/kotlin/weather/viewmodel/view/ForecastAdapter.kt
@@ -1,15 +1,58 @@
 package com.cornucopia.kotlin.weather.viewmodel.view
 
 import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.cornucopia.kotlin.R
+import com.cornucopia.kotlin.weather.repository.ListWeather
+import kotlinx.android.synthetic.main.layout_forecast_item.view.*
 import java.util.Date;
 
 /**
  * Created by thom on 4/2/2018.
  */
-class ForecastAdapter {
+class ForecastAdapter : RecyclerView.Adapter<ForecastAdapter.ForecastHolder> {
 
-    constructor(context: Context, clickHandler: ForecastAdapterOnItemClickHandler) {
+    var mWeathers: List<ListWeather> = listOf()
 
+    var mContext: Context;
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForecastHolder {
+        val inflate = mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val itemView = inflate.inflate(R.layout.layout_forecast_item, parent, false)
+        return ForecastHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return mWeathers!!.count()
+    }
+
+    override fun onBindViewHolder(holder: ForecastHolder, position: Int) {
+        var weather = mWeathers.get(position)
+
+        holder.itemDate.setText("" + weather.date)
+        holder.itemInterval.setText("" + weather.min + " ~ " + weather.max)
+    }
+
+
+    constructor(context: Context, weathers: List<ListWeather>,
+                clickHandler: ForecastAdapterOnItemClickHandler) {
+        mContext = context
+        mWeathers = weathers
+    }
+
+    class ForecastHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+
+        var itemDate : TextView
+        var itemInterval : TextView
+
+        init {
+            itemDate = itemView!!.findViewById(R.id.tv_forecast_date)
+            itemInterval = itemView!!.tv_forecast_interval
+        }
     }
 
     interface ForecastAdapterOnItemClickHandler {
